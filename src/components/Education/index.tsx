c0d7859beb76052d528e881e50e9e2b891a044b9
Oussama Sallak <osallak@student.1337.ma>
2025-01-31 16:47:41 +0100

    refactor(ui): Remove unused Framer Motion variants in Education component

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -37,16 +37,6 @@ const containerVariants = {
   },
 };
 
-const itemVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.5,
-    },
-  },
-};
 
 const Education = () => {
   return (
